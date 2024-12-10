d259f65a0aa12e9ae22f2fa662dfdaf57ea0c479
asherorr <103453685+asherorr@users.noreply.github.com>
2024-12-10 17:13:30 +0000

    This commit will modify the code to abide by Ruby style gudelines.

--- lesson_5_oop/oo_ttt.rb
+++ lesson_5_oop/oo_ttt.rb
@@ -1,12 +1,12 @@
-require "pry"
+# frozen_string_literal: true
 
-class Board
+class Board # rubocop:disable Style/Documentation
   WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
-  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
-  [[1, 5, 9], [3, 5, 7]]              # diagonals
-  
+                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
+                  [[1, 5, 9], [3, 5, 7]]              # diagonals
+
   def initialize
-  	@squares = {}
+    @squares = {}
     reset
   end
 
@@ -14,24 +14,28 @@ class Board
     @squares[key].marker = marker
   end
 
+  # rubocop:disable Metrics/AbcSize
+  # rubocop:disable Metrics/MethodLength
   def draw
-    puts ""
-    puts "     |     |"
+    puts ''
+    puts '     |     |'
     puts "  #{@squares[1]}  |  #{@squares[2]}  |  #{@squares[3]}"
-    puts "     |     |"
-    puts "-----+-----+-----"
-    puts "     |     |"
+    puts '     |     |'
+    puts '-----+-----+-----'
+    puts '     |     |'
     puts "  #{@squares[4]}  |  #{@squares[5]}  |  #{@squares[6]}"
-    puts "     |     |"
-    puts "-----+-----+-----"
-    puts "     |     |"
+    puts '     |     |'
+    puts '-----+-----+-----'
+    puts '     |     |'
     puts "  #{@squares[7]}  |  #{@squares[8]}  |  #{@squares[9]}"
-    puts "     |     |"
-    puts ""
+    puts '     |     |'
+    puts ''
   end
+  # rubocop:enable Metrics/AbcSize
+  # rubocop:enable Metrics/MethodLength
 
   def unmarked_keys
-    @squares.keys.select {|key| @squares[key].unmarked? }
+    @squares.keys.select { |key| @squares[key].unmarked? }
   end
 
   def full?
@@ -50,19 +54,17 @@ class Board
     squares.collect(&:marker).count(TTTGame::COMPUTER_MARKER)
   end
 
-  #returns winning marker or nil
+  # returns winning marker or nil
   def winning_marker
     WINNING_LINES.each do |line|
       squares = @squares.values_at(*line)
-      if three_identical_markers?(squares) 
-        return squares.first.marker           
-      end
+      return squares.first.marker if three_identical_markers?(squares)
     end
     nil
   end
 
   def reset
-    (1..9).each {|key| @squares[key] = Square.new}
+    (1..9).each { |key| @squares[key] = Square.new }
   end
 
   private
@@ -70,19 +72,20 @@ class Board
   def three_identical_markers?(squares)
     markers = squares.select(&:marked?).collect(&:marker)
     return false if markers.size != 3
+
     markers.min == markers.max
   end
 end
 
-class Square
-  INITIAL_MARKER = " "
+class Square # rubocop:disable Style/Documentation
+  INITIAL_MARKER = ' '
 
   attr_accessor :marker
-  
-  def initialize(marker=INITIAL_MARKER)
+
+  def initialize(marker = INITIAL_MARKER)
     @marker = marker
   end
-  
+
   def to_s
     @marker
   end
@@ -96,38 +99,47 @@ class Square
   end
 end
 
-class Player
+class Player # rubocop:disable Style/Documentation
   attr_reader :marker
-  
+
   def initialize(marker)
     @marker = marker
   end
 end
 
-class TTTGame
-  HUMAN_MARKER = "X"
-  COMPUTER_MARKER = "O"
+class TTTGame # rubocop:disable Style/Documentation
+  HUMAN_MARKER = 'X'
+  COMPUTER_MARKER = 'O'
   FIRST_TO_MOVE = HUMAN_MARKER
 
   attr_reader :board, :human, :computer
 
-  def play
-    clear_screen
-    display_welcome_message
-
+  def player_move
     loop do
-    display_board
+      current_player_moves
+      break if board.someone_won? || board.full?
 
-      loop do
-        current_player_moves
-        break if board.someone_won? || board.full?
-        clear_screen_and_display_board if human_turn?
-      end
+      clear_screen_and_display_board if human_turn?
+    end
+  end
+
+  def play
+    clear
+    display_welcome_message
+    main_game
+    display_goodbye_message
+  end
+
+  def main_game
+    loop do
+      display_board
+      player_move
       display_result
       break unless play_again?
+
       reset
+      display_play_again_message
     end
-    display_goodbye_message
   end
 
   private
@@ -138,16 +150,16 @@ class TTTGame
     @computer = Player.new(COMPUTER_MARKER)
     @current_marker = FIRST_TO_MOVE
   end
-  
+
   def display_welcome_message
-    puts "Welcome to Tic Tac Toe!"
-    puts ""
+    puts 'Welcome to Tic Tac Toe!'
+    puts ''
   end
 
   def display_goodbye_message
-    puts "Thanks for playing Tic Tac Toe! Goodbye!"
+    puts 'Thanks for playing Tic Tac Toe! Goodbye!'
   end
-  
+
   def display_board
     puts "You're a #{human.marker}. Computer is a #{computer.marker}."
     board.draw
@@ -171,16 +183,17 @@ class TTTGame
   def human_turn?
     @current_marker == HUMAN_MARKER
   end
-  
+
   def human_moves
     puts "Choose a square (#{board.unmarked_keys.join(', ')}): "
     square = nil
     loop do
       square = gets.chomp.to_i
       break if board.unmarked_keys.include?(square)
+
       puts "Sorry, that's not a valid choice."
     end
-    
+
     board.set_square_at(square, human.marker)
   end
 
@@ -188,15 +201,14 @@ class TTTGame
     board.set_square_at(board.unmarked_keys.sample, computer.marker)
   end
 
-  
   def display_result
     clear_screen_and_display_board
 
     case board.winning_marker
     when human.marker
-      puts "You won!"
+      puts 'You won!'
     when computer.marker
-      puts "Computer won!"
+      puts 'Computer won!'
     else
       puts "It's a tie!"
     end
@@ -205,13 +217,14 @@ class TTTGame
   def play_again?
     answer = nil
     loop do
-      puts "Would you like to play again? (y/n)"
+      puts 'Would you like to play again? (y/n)'
       answer = gets.chomp.downcase
-      break if %w(y n).include? answer
-      puts "Sorry, must be y or n."
+      break if %w[y n].include? answer
+
+      puts 'Sorry, must be y or n.'
     end
 
-    answer == "y"
+    answer == 'y'
   end
 
   def clear_screen
@@ -223,7 +236,7 @@ class TTTGame
     @current_marker = FIRST_TO_MOVE
     clear_screen
     puts "Let's play again!"
-    puts ""
+    puts ''
   end
 end
 
