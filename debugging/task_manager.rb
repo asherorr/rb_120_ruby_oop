5c05b32b2f8489d385450113ee10777511b88f1d
asherorr <103453685+asherorr@users.noreply.github.com>
2024-12-31 00:42:14 +0000

    This commit will add the solutions I wrote for questions 8 and 9 in the RB120 Debugging exercise problem set.

---
+++ debugging/task_manager.rb
@@ -0,0 +1,164 @@
+#The following code produces an error.
+#Find the bug and fix it.
+
+# class TaskManager
+#   attr_reader :owner
+#   attr_accessor :tasks
+
+#   def initialize(owner)
+#     @owner = owner
+#     @tasks = []
+#   end
+
+#   def add_task(name, priority=:normal)
+#     task = Task.new(name, priority)
+#     tasks.push(task)
+#   end
+
+#   def complete_task(task_name)
+#     completed_task = nil
+
+#     tasks.each do |task|
+#       completed_task = task if task.name == task_name
+#     end
+
+#     if completed_task
+#       tasks.delete(completed_task)
+#       puts "Task '#{completed_task.name}' complete! Removed from list."
+#     else
+#       puts "Task not found."
+#     end
+#   end
+
+#   def display_all_tasks
+#     display(tasks)
+#   end
+
+#   def display_high_priority_tasks
+#     tasks = tasks.select do |task|
+#       task.priority == :high
+#     end
+
+#     display(tasks)
+#   end
+
+#   private
+
+#   def display(tasks)
+#     puts "--------"
+#     tasks.each do |task|
+#       puts task
+#     end
+#     puts "--------"
+#   end
+# end
+
+# class Task
+#   attr_accessor :name, :priority
+
+#   def initialize(name, priority=:normal)
+#     @name = name
+#     @priority = priority
+#   end
+
+#   def to_s
+#     "[" + sprintf("%-6s", priority) + "] #{name}"
+#   end
+# end
+
+# valentinas_tasks = TaskManager.new('Valentina')
+
+# valentinas_tasks.add_task('pay bills', :high)
+# valentinas_tasks.add_task('read OOP book')
+# valentinas_tasks.add_task('practice Ruby')
+# valentinas_tasks.add_task('run 5k', :low)
+
+# valentinas_tasks.complete_task('read OOP book')
+
+# valentinas_tasks.display_all_tasks
+# valentinas_tasks.display_high_priority_tasks
+
+#The issue lies in the local variable assigmment `tasks` on line 38.
+#When we're reassigning an instance variable within an instance method, it must be preced with `self.`
+#Otherwise, Ruby assumes we're referring to the local variable `tasks`, which hasn't been assigned
+#a value yet, which makes its value `nil` upon invoking the method.
+
+#We can also change the local variable to be more descriptive, like `high_priority_tasks`
+# (I made this change below.)
+
+class TaskManager
+  attr_reader :owner
+  attr_accessor :tasks
+
+  def initialize(owner)
+    @owner = owner
+    @tasks = []
+  end
+
+  def add_task(name, priority=:normal)
+    task = Task.new(name, priority)
+    tasks.push(task)
+  end
+
+  def complete_task(task_name)
+    completed_task = nil
+
+    tasks.each do |task|
+      completed_task = task if task.name == task_name
+    end
+
+    if completed_task
+      tasks.delete(completed_task)
+      puts "Task '#{completed_task.name}' complete! Removed from list."
+    else
+      puts "Task not found."
+    end
+  end
+
+  def display_all_tasks
+    display(tasks)
+  end
+
+  def display_high_priority_tasks
+    high_priority_tasks = tasks.select do |task|
+      task.priority == :high
+    end
+
+    display(high_priority_tasks)
+  end
+
+  private
+
+  def display(tasks)
+    puts "--------"
+    tasks.each do |task|
+      puts task
+    end
+    puts "--------"
+  end
+end
+
+class Task
+  attr_accessor :name, :priority
+
+  def initialize(name, priority=:normal)
+    @name = name
+    @priority = priority
+  end
+
+  def to_s
+    "[" + sprintf("%-6s", priority) + "] #{name}"
+  end
+end
+
+valentinas_tasks = TaskManager.new('Valentina')
+
+valentinas_tasks.add_task('pay bills', :high)
+valentinas_tasks.add_task('read OOP book')
+valentinas_tasks.add_task('practice Ruby')
+valentinas_tasks.add_task('run 5k', :low)
+
+valentinas_tasks.complete_task('read OOP book')
+
+valentinas_tasks.display_all_tasks
+valentinas_tasks.display_high_priority_tasks
